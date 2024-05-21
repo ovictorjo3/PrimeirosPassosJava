6844c7de3e17b361aad758b0096c3f9b299fd49e
Joao Victor <jsousa.1998@alunos.utfpr.edu.br>
2024-05-21 18:22:37 -0400

    exemplo de concatemação

---
+++ src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Este é o Screen Match");
+
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int ano = 2022;
+
+        System.out.println("Ano de Lançamento: " + ano);
+
+        boolean incluidoNoPlano = true;
+
+        
+
+
+    }
+}
